02dc54bdcbb45b7bd8550e7d6bf13069724a594e
masato sasaki <m.tobcreation@gmail.com>
2021-11-10 23:30:10 +0900

    mail送信追加

--- line.py
+++ line.py
@@ -7,7 +7,31 @@ import json
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 import sys
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+import base64
+from email.mime.text import MIMEText
+from apiclient import errors
 
+# 2. メール本文の作成
+def create_message(sender, to, subject, message_text):
+    message = MIMEText(message_text)
+    message['to'] = to
+    message['from'] = sender
+    message['subject'] = subject
+    encode_message = base64.urlsafe_b64encode(message.as_bytes())
+    return {'raw': encode_message.decode()}
+# 3. メール送信の実行
+def send_message(service, user_id, message):
+    try:
+        message = (service.users().messages().send(userId=user_id, body=message).execute())
+        # print('Message Id: %s' % message['id'])
+        return message
+    except errors.HttpError as error:
+        print('An error occurred: %s' % error)
 
 def line_send_message(name,property_data):
     send_text = f'{name}様\n\nこんにちは。\n'
@@ -101,14 +125,42 @@ for p in conditions_list:
     if len(sc_list) >0:
         send_data = [{i: k for i,k in zip(sc_list_title,s)} for s in sc_list]
         send_data = [[[i,d[i]] for i in s.send_categories[c_kind]]for d in send_data]
-        # -------------------------------------------------------------------------------------------
 
-        # 画像をLINEで送付-----------------------------------------------------------------------------
 
-        # -------------------------------------------------------------------------------------------
-        line_bot_api = LineBotApi(key.LINE_ACCESS_TOKEN)
-        user_id = p["lineID"]
-        profile = line_bot_api.get_profile(user_id)
-        user_name = profile.display_name
-        messages = TextSendMessage(text=line_send_message(user_name,send_data))
-        line_bot_api.push_message(user_id, messages=messages)
+        if p["配信方法"] == "LINE":
+            # -------------------------------------------------------------------------------------------
+
+            # 画像をLINEで送付-----------------------------------------------------------------------------
+
+            # -------------------------------------------------------------------------------------------
+            line_bot_api = LineBotApi(key.LINE_ACCESS_TOKEN)
+            user_id = p["lineID"]
+            profile = line_bot_api.get_profile(user_id)
+            user_name = profile.display_name
+            messages = TextSendMessage(text=line_send_message(user_name,send_data))
+            line_bot_api.push_message(user_id, messages=messages)
+        elif p["配信方法"] == "MAIL":
+            # 4. メインとなる処理
+            # 5. アクセストークンの取得
+            creds = None
+            if os.path.exists('token.pickle'):
+                with open('token.pickle', 'rb') as token:
+                    creds = pickle.load(token)
+            if not creds or not creds.valid:
+                if creds and creds.expired and creds.refresh_token:
+                    creds.refresh(Request())
+                else:
+                    flow = InstalledAppFlow.from_client_secrets_file(
+                        'js/credentials.json', key.SCOPES)
+                    creds = flow.run_local_server()
+                with open('token.pickle', 'wb') as token:
+                    pickle.dump(creds, token)
+            service = build('gmail', 'v1', credentials=creds)
+            # 6. メール本文の作成
+            sender = 'm.tobcreation@gmail.com'
+            to = p["mail"]
+            subject = '希望条件に近い不動産情報が見つかりました。'
+            message_text = line_send_message(p["お客様名"],send_data)
+            message = create_message(sender, to, subject, message_text)
+            # 7. Gmail APIを呼び出してメール送信
+            send_message(service, 'me', message)
