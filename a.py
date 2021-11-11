0776459b4288657918d5fbeedd7b4450e5d7bd06
masato sasaki <m.tobcreation@gmail.com>
2021-11-11 19:03:41 +0900

    no message

--- a.py
+++ a.py
@@ -4,7 +4,4 @@ dt_now = datetime.datetime.now()
 
 print(dt_now)
 # 2019-02-04 21:04:15.412854
-
 print(dt_now.strftime('%Y-%m-%d'))
-print(type(dt_now))
-# <class 'datetime.datetime'>
--- line.py
+++ line.py
@@ -17,6 +17,7 @@ from linebot.models import TextSendMessage
 import json
 import base64
 from apiclient import errors
+import datetime
 from dotenv import load_dotenv
 
 import setting as s
@@ -35,6 +36,8 @@ SCOPES_GMAIL = ['https://www.googleapis.com/auth/gmail.send']
 
 if "env" in os.environ.keys():
     mode = 0
+    dt_now = datetime.datetime.now()
+    dt_now = dt_now.strftime('%Y-%m-%d')
     token_json = os.environ["token"]
     secret = json.loads(os.environ["credentials"])
     flow_secret = InstalledAppFlow.from_client_config(secret, SCOPES_GMAIL)
@@ -45,7 +48,12 @@ if "env" in os.environ.keys():
     CREDENTIALS = ServiceAccountCredentials.from_json_keyfile_dict(GOOGLE_JSON, SCOPES)
 
 else:
-    mode = 1
+    mode = 2
+    if mode == 1:
+        dt_now = datetime.datetime.now()
+        dt_now = dt_now.strftime('%Y-%m-%d')
+    elif mode ==2:
+        dt_now ="2021-11-09"
     token_json = os.path.exists('token.json')
     secret = 'js/credentials.json'
     flow_secret = InstalledAppFlow.from_client_secrets_file(secret, SCOPES_GMAIL)
@@ -147,153 +155,155 @@ gc = gspread.authorize(credentials) #OAuth2の資格情報を使用してGoogle 
 
 #driveフォルダ内のファイル名一覧取得
 sc_file_list = [f['title'] for f in drive.ListFile({'q': '"{}" in parents'.format(FOLDER_ID)}).GetList()]
+if dt_now in sc_file_list:
+    #該当ファイルの物件情報取得
+    ws_sc_file = gc.open(dt_now)#該当ファイルを開く
+    sc_all_list = {} #物件情報を格納するリスト
+    sc_all_title = {} #物件情報のキー格納
+    for i in s.kind: #種類別に物件情報を格納
+        sc_list = ws_sc_file.worksheet(i).get_all_values()
+        sc_all_title[i] = sc_list[0]
+        del sc_list[0]
+        sc_all_list[i] = sc_list
 
-#該当ファイルの物件情報取得
-ws_sc_file = gc.open("2021-11-09")#該当ファイルを開く
-sc_all_list = {} #物件情報を格納するリスト
-sc_all_title = {} #物件情報のキー格納
-for i in s.kind: #種類別に物件情報を格納
-    sc_list = ws_sc_file.worksheet(i).get_all_values()
-    sc_all_title[i] = sc_list[0]
-    del sc_list[0]
-    sc_all_list[i] = sc_list
+    #ユーザーの希望条件リストを習得
+    ws_conditions_list = gc.open_by_key(SPREADSHEET_KEY).worksheet("希望条件") #スプレッドシート【LINE物件情報自動通知】の"希望条件"シートを開く
+    conditions_list = ws_conditions_list.get_all_values() #希望条件シートの全データ取得
+    conditions_list = [{k:i for i,k in zip(c,conditions_list[0])} for c in conditions_list] #辞書型に変換
+    del conditions_list[0] #1行目削除
+    conditions_list = [i for i in conditions_list if i["配信"] == "●" ] #配信設定しているものだけにフィルター
 
-#ユーザーの希望条件リストを習得
-ws_conditions_list = gc.open_by_key(SPREADSHEET_KEY).worksheet("希望条件") #スプレッドシート【LINE物件情報自動通知】の"希望条件"シートを開く
-conditions_list = ws_conditions_list.get_all_values() #希望条件シートの全データ取得
-conditions_list = [{k:i for i,k in zip(c,conditions_list[0])} for c in conditions_list] #辞書型に変換
-del conditions_list[0] #1行目削除
-conditions_list = [i for i in conditions_list if i["配信"] == "●" ] #配信設定しているものだけにフィルター
+    #配信希望ユーザーが1人以上の場合実行
+    if len(conditions_list) > 0:
+        #ユーザーの希望条件リストの数だけ繰り返し
+        for p in conditions_list:
+            print(f'{p["お客様名"]}様の希望物件検索を開始します。')
+            #変数の設定
+            c_kind = p["物件種別"] #物件種別
+            sc_list = sc_all_list[c_kind] #物件種別の物件情報一覧
+            sc_list_title = sc_all_title[c_kind] #物件情報一覧のキー
+            filter_col = s.filter_col[c_kind] #物件情報のインデックス番号
 
-#配信希望ユーザーが1人以上の場合実行
-if len(conditions_list) > 0:
-    #ユーザーの希望条件リストの数だけ繰り返し
-    for p in conditions_list:
-        print(f'{p["お客様名"]}様の希望物件検索を開始します。')
-        #変数の設定
-        c_kind = p["物件種別"] #物件種別
-        sc_list = sc_all_list[c_kind] #物件種別の物件情報一覧
-        sc_list_title = sc_all_title[c_kind] #物件情報一覧のキー
-        filter_col = s.filter_col[c_kind] #物件情報のインデックス番号
+            #希望条件でフィルター---------------------------------------------------------------------------------------
 
-        #希望条件でフィルター---------------------------------------------------------------------------------------
-
-        #エリアの検索方法で分岐
-        #地域から選ぶ場合
-        print(f'{c_kind}の物件情報は{len(sc_list)}件です')
-        if p["エリアの検索方法"] == "地域から選ぶ":
-            if p["地域"] != "": #地域が空白じゃなければ
-                print(f'エリアを地域から検索します。')
-                sc_list = [i for i in sc_list if p["地域"] in i[filter_col[0]]]
-                print(f'{p["地域"]}に該当する物件は{len(sc_list)}件でした。')
-            else:
-                print(f'エリアの条件設定はありません。')
-        #路線から選ぶ場合
-        elif p["エリアの検索方法"] == "路線から選ぶ":
-            if p["路線"] != "": #路線が空白じゃなければ
-                print(f'エリアを路線から検索します。')
-                sc_list = [i for i in sc_list if p["路線"] in i[filter_col[1]]]
-                print(f'{p["路線"]}に該当する物件は{len(sc_list)}件でした。')
-                if p["駅名"] != "":
-                    print(f'駅名で絞り込みをします。')
-                    sc_list = [i for i in sc_list if p["駅名"] in i[filter_col[1]]]
-                    sc_list = [i for i in sc_list if "バス" not in i[filter_col[1]].split("」")[1]]
-                    print(f'{p["駅名"]}に該当する物件は{len(sc_list)}件でした。')
+            #エリアの検索方法で分岐
+            #地域から選ぶ場合
+            print(f'{c_kind}の物件情報は{len(sc_list)}件です')
+            if p["エリアの検索方法"] == "地域から選ぶ":
+                if p["地域"] != "": #地域が空白じゃなければ
+                    print(f'エリアを地域から検索します。')
+                    sc_list = [i for i in sc_list if p["地域"] in i[filter_col[0]]]
+                    print(f'{p["地域"]}に該当する物件は{len(sc_list)}件でした。')
                 else:
-                    print(f'駅名の条件設定はありません。')
-            else:
-                print(f'路線の条件設定はありません。')
-            if p["分数（徒歩）"] != "":
-                print(f'駅までの分数で絞り込みをします。')
-                sc_list = [i for i in sc_list if int(p["分数（徒歩）"]) >= remove_walk(i[filter_col[1]])]
-                print(f'徒歩{p["分数（徒歩）"]}以内に該当する物件は{len(sc_list)}件でした。')
-            else:
-                print(f'分数の条件設定はありません。')
-
-        #上限価格でフィルター
-        if p["上限価格（万円）"] != "":
-            print(f'上限価格で検索します。')
-            sc_list = [i for i in sc_list if int(p["上限価格（万円）"]) > remove_price(i[filter_col[2]])]
-            print(f'{p["上限価格（万円）"]}万円以下に該当する物件は{len(sc_list)}件でした。')
-        else:
-            print(f'上限価格の条件設定はありません。')
-
-        #下限平米数でフィルター
-        if p["下限平米数（㎡）"] != "":
-            print(f'下限平米数で検索します。')
-            sc_list = [i for i in sc_list if int(p["下限平米数（㎡）"]) < remove_m(i[filter_col[3]],c_kind)]
-            print(f'{p["下限平米数（㎡）"]}㎡以上に該当する物件は{len(sc_list)}件でした。')
-        else:
-            print(f'下限平米数の条件設定はありません。')
-        #希望条件でフィルタここまでー---------------------------------------------------------------------------------------
-
-        #条件に合致する物件があるか確認
-        if len(sc_list) >0:
-            print(f'希望条件に合致する物件が{len(sc_list)}件ありました')
-            send_data = [{i: k for i,k in zip(sc_list_title,s)} for s in sc_list]
-            send_data = [[[i,d[i]] for i in s.send_categories[c_kind]]for d in send_data]
-
-            #配信方法で分岐
-
-            #LINEの場合
-            if p["配信方法"] == "LINE":
-                if p["lineID"]:
-                # -------------------------------------------------------------------------------------------
-                # LINEで送付----------------------------------------------------------------------------------
-                # -------------------------------------------------------------------------------------------
-                    line_bot_api = LineBotApi(LINE_ACCESS_TOKEN)
-                    user_id = p["lineID"]
-                    profile = line_bot_api.get_profile(user_id)
-                    user_name = profile.display_name
-                    messages = TextSendMessage(text=line_send_message(user_name,send_data))
-                    line_bot_api.push_message(user_id, messages=messages)
-                    print(f'{len(sc_list)}件の物件情報を{p["お客様名"]}様にLINEで送付しました')
-                # -------------------------------------------------------------------------------------------
-                # LINEで送付ここまで---------------------------------------------------------------------------
-                # -------------------------------------------------------------------------------------------
+                    print(f'エリアの条件設定はありません。')
+            #路線から選ぶ場合
+            elif p["エリアの検索方法"] == "路線から選ぶ":
+                if p["路線"] != "": #路線が空白じゃなければ
+                    print(f'エリアを路線から検索します。')
+                    sc_list = [i for i in sc_list if p["路線"] in i[filter_col[1]]]
+                    print(f'{p["路線"]}に該当する物件は{len(sc_list)}件でした。')
+                    if p["駅名"] != "":
+                        print(f'駅名で絞り込みをします。')
+                        sc_list = [i for i in sc_list if p["駅名"] in i[filter_col[1]]]
+                        sc_list = [i for i in sc_list if "バス" not in i[filter_col[1]].split("」")[1]]
+                        print(f'{p["駅名"]}に該当する物件は{len(sc_list)}件でした。')
+                    else:
+                        print(f'駅名の条件設定はありません。')
                 else:
-                    print(f'{p["お客様名"]}様のLINE IDが設定されていません。')
-
-            #MAILの場合
-            elif p["配信方法"] == "MAIL":
-                if p["mail"]:
-                # -------------------------------------------------------------------------------------------
-                # MAILで送付----------------------------------------------------------------------------------
-                # -------------------------------------------------------------------------------------------
-                    creds = None
-                    if token_json:
-                        if mode == 0:
-                            tokenFile = json.loads(os.environ["token"])
-                            with open('token.json', 'w') as f:
-                                json.dump(tokenFile, f, ensure_ascii=False)
-                        creds = Credentials.from_authorized_user_file('token.json', SCOPES_GMAIL)
-                    if not creds or not creds.valid:
-                        if creds and creds.expired and creds.refresh_token:
-                            creds.refresh(Request())
-                        else:
-                            flow = flow_secret
-                            creds = flow.run_local_server()
-                        with open('token.json', 'w') as token:
-                            token.write(creds.to_json())
-                    service = build('gmail', 'v1', credentials=creds)
-                    # 6. メール本文の作成
-                    sender = ''
-                    to = p["mail"]
-                    subject = '希望条件に近い不動産情報が見つかりました。'
-                    message_text = line_send_message(p["お客様名"],send_data)
-                    message = create_message(sender, to, subject, message_text)
-                    # 7. Gmail APIを呼び出してメール送信
-                    send_message(service, 'me', message)
-                    print(f'{len(sc_list)}件の物件情報を{p["お客様名"]}様にMAILで送付しました')
-                # -------------------------------------------------------------------------------------------
-                # MAILで送付ここまで---------------------------------------------------------------------------
-                # -------------------------------------------------------------------------------------------
+                    print(f'路線の条件設定はありません。')
+                if p["分数（徒歩）"] != "":
+                    print(f'駅までの分数で絞り込みをします。')
+                    sc_list = [i for i in sc_list if int(p["分数（徒歩）"]) >= remove_walk(i[filter_col[1]])]
+                    print(f'徒歩{p["分数（徒歩）"]}以内に該当する物件は{len(sc_list)}件でした。')
                 else:
-                    print(f'{p["お客様名"]}様のMAILアドレスが設定されていません。')
+                    print(f'分数の条件設定はありません。')
 
+            #上限価格でフィルター
+            if p["上限価格（万円）"] != "":
+                print(f'上限価格で検索します。')
+                sc_list = [i for i in sc_list if int(p["上限価格（万円）"]) > remove_price(i[filter_col[2]])]
+                print(f'{p["上限価格（万円）"]}万円以下に該当する物件は{len(sc_list)}件でした。')
             else:
-                print(f'{p["お客様名"]}様の配信方法が設定されていません。')
-        else:
-            print(f'{p["お客様名"]}様の希望に合致する物件がありませんでした。')
+                print(f'上限価格の条件設定はありません。')
+
+            #下限平米数でフィルター
+            if p["下限平米数（㎡）"] != "":
+                print(f'下限平米数で検索します。')
+                sc_list = [i for i in sc_list if int(p["下限平米数（㎡）"]) < remove_m(i[filter_col[3]],c_kind)]
+                print(f'{p["下限平米数（㎡）"]}㎡以上に該当する物件は{len(sc_list)}件でした。')
+            else:
+                print(f'下限平米数の条件設定はありません。')
+            #希望条件でフィルタここまでー---------------------------------------------------------------------------------------
+
+            #条件に合致する物件があるか確認
+            if len(sc_list) >0:
+                print(f'希望条件に合致する物件が{len(sc_list)}件ありました')
+                send_data = [{i: k for i,k in zip(sc_list_title,s)} for s in sc_list]
+                send_data = [[[i,d[i]] for i in s.send_categories[c_kind]]for d in send_data]
+
+                #配信方法で分岐
+
+                #LINEの場合
+                if p["配信方法"] == "LINE":
+                    if p["lineID"]:
+                    # -------------------------------------------------------------------------------------------
+                    # LINEで送付----------------------------------------------------------------------------------
+                    # -------------------------------------------------------------------------------------------
+                        line_bot_api = LineBotApi(LINE_ACCESS_TOKEN)
+                        user_id = p["lineID"]
+                        profile = line_bot_api.get_profile(user_id)
+                        user_name = profile.display_name
+                        messages = TextSendMessage(text=line_send_message(user_name,send_data))
+                        line_bot_api.push_message(user_id, messages=messages)
+                        print(f'{len(sc_list)}件の物件情報を{p["お客様名"]}様にLINEで送付しました')
+                    # -------------------------------------------------------------------------------------------
+                    # LINEで送付ここまで---------------------------------------------------------------------------
+                    # -------------------------------------------------------------------------------------------
+                    else:
+                        print(f'{p["お客様名"]}様のLINE IDが設定されていません。')
+
+                #MAILの場合
+                elif p["配信方法"] == "MAIL":
+                    if p["mail"]:
+                    # -------------------------------------------------------------------------------------------
+                    # MAILで送付----------------------------------------------------------------------------------
+                    # -------------------------------------------------------------------------------------------
+                        creds = None
+                        if token_json:
+                            if mode == 0:
+                                tokenFile = json.loads(os.environ["token"])
+                                with open('token.json', 'w') as f:
+                                    json.dump(tokenFile, f, ensure_ascii=False)
+                            creds = Credentials.from_authorized_user_file('token.json', SCOPES_GMAIL)
+                        if not creds or not creds.valid:
+                            if creds and creds.expired and creds.refresh_token:
+                                creds.refresh(Request())
+                            else:
+                                flow = flow_secret
+                                creds = flow.run_local_server()
+                            with open('token.json', 'w') as token:
+                                token.write(creds.to_json())
+                        service = build('gmail', 'v1', credentials=creds)
+                        # 6. メール本文の作成
+                        sender = ''
+                        to = p["mail"]
+                        subject = '希望条件に近い不動産情報が見つかりました。'
+                        message_text = line_send_message(p["お客様名"],send_data)
+                        message = create_message(sender, to, subject, message_text)
+                        # 7. Gmail APIを呼び出してメール送信
+                        send_message(service, 'me', message)
+                        print(f'{len(sc_list)}件の物件情報を{p["お客様名"]}様にMAILで送付しました')
+                    # -------------------------------------------------------------------------------------------
+                    # MAILで送付ここまで---------------------------------------------------------------------------
+                    # -------------------------------------------------------------------------------------------
+                    else:
+                        print(f'{p["お客様名"]}様のMAILアドレスが設定されていません。')
+
+                else:
+                    print(f'{p["お客様名"]}様の配信方法が設定されていません。')
+            else:
+                print(f'{p["お客様名"]}様の希望に合致する物件がありませんでした。')
+    else:
+        print("配信希望ユーザーがいませんでした。物件情報自動通知処理を終了します。")
 else:
-    print("配信希望ユーザーがいませんでした。物件情報自動通知処理を終了します。")
+    print(f'{dt_now}のファイルが見つかりませんでした。処理を終了します。')
